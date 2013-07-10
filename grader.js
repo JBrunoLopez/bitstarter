3757abff4b3a4e8827c1104095f9e4288c9af863
Bruno Lopez <JBruno.Lopez@gmail.com>
2013-07-10 01:17:13 -0400

    Now my grader can check the html gotten from a URL

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,6 +38,20 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var checkHtmlFromUrl = function(url) {
+    return rest.get(url).on('complete', function(result) {
+        var html = '';
+        if(result instanceof Error) {
+            console.log("URL not found.");
+        } else {
+            html = result;
+        }
+        checkJson = checkHtmlFile(cheerio.load(html), program.checks)
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    });
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -44,8 +60,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(cheerioFunctor, checksfile) {
+    $ = cheerioFunctor;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +81,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL to html_file>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url) {
+        checkHtmlFromUrl(program.url);
+    } else {
+        var checkJson = checkHtmlFile(cheerioHtmlFile(program.file), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
